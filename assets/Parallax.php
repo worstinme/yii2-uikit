37205ba107968a83668ed24a5cc231e72744c724
Evgeny Zakirov <box@flyleaf.su>
2015-10-19 07:14:22 +0500

    parallax added

---
+++ assets/Parallax.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace worstinme\uikit\assets;
+
+use yii\web\AssetBundle;
+
+class Parallax extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/uikit';
+
+    public $css = [
+        
+    ];
+
+    public $js = [
+        'js'=>'js/components/parallax.min.js',
+    ];
+
+    public $depends = [
+        'worstinme\uikit\UikitAsset',
+    ];
+}
