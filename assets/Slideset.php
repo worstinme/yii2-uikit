bf78753cc7f89097507e93a5d88523b7e0f2dc18
Evgeny Zakirov <box@flyleaf.su>
2016-03-29 14:48:47 +0500

    +slideset

---
+++ assets/Slideset.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace worstinme\uikit\assets;
+
+use yii\web\AssetBundle;
+
+class Slideset extends AssetBundle
+{
+     public $sourcePath = '@vendor/bower/uikit';
+
+    public $css = [
+        'css/components/slidenav.min.css',
+    ];
+
+    public $js = [
+        'js/components/slideset.min.js',
+    ];
+
+    public $depends = [
+        'worstinme\uikit\UikitAsset',
+    ];
+}
