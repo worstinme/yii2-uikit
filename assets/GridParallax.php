084e93183203175a72223535b0e0f0e75a330a3d
Evgeny Zakirov <box@flyleaf.su>
2016-04-25 01:32:50 +0500

    grid parallax

---
+++ assets/GridParallax.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace worstinme\uikit\assets;
+
+use yii\web\AssetBundle;
+
+class GridParallax extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/uikit';
+
+    public $css = [
+        
+    ];
+
+    public $js = [
+        'js/components/grid-parallax.min.js',
+    ];
+
+    public $depends = [
+        'worstinme\uikit\UikitAsset',
+    ];
+}
