793d7783e8f4570cbf650c19a12f9b4f73ed44bc
Evgeny Zakirov <box@flyleaf.su>
2016-05-29 03:00:55 +0500

    + tooltip asset

---
+++ assets/Tooltip.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace worstinme\uikit\assets;
+
+use yii\web\AssetBundle;
+
+class Tooltip extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/uikit';
+
+    public $css = [
+        'css/components/tooltip.min.css',
+    ];
+
+    public $js = [
+        'js/components/tooltip.min.js',
+    ];
+
+    public $depends = [
+        'worstinme\uikit\UikitAsset',
+    ];
+}
