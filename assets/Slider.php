bc3ff2d50a649d1d882a64c790fed2b266229488
Evgeny Zakirov <box@flyleaf.su>
2016-05-29 02:59:41 +0500

    + slider asset

---
+++ assets/Slider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace worstinme\uikit\assets;
+
+use yii\web\AssetBundle;
+
+class Slider extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/uikit';
+
+    public $css = [
+        'css/components/slidenav.min.css',
+        'css/components/slider.min.css',
+        'css/components/dotnav.min.css',
+    ];
+
+    public $js = [
+        'js/components/slider.min.js',
+    ];
+
+    public $depends = [
+        'worstinme\uikit\UikitAsset',
+    ];
+}
