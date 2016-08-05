7f732af0f7c2ac21a5d282a27f6b23b9ce69913f
Evgeny Zakirov <box@flyleaf.su>
2016-08-05 11:23:32 +0500

    grid view margint top remove

--- widgets/GridView.php
+++ widgets/GridView.php
@@ -5,8 +5,8 @@ namespace worstinme\uikit\widgets;
 class GridView extends \yii\grid\GridView
 {
 
-	public $tableOptions = ['class' => 'uk-table uk-form uk-table-condensed uk-table-hover uk-margin-top'];
-	public $summaryOptions = ['class'=>'uk-text-center'];
+	public $tableOptions = ['class' => 'uk-table uk-form uk-table-condensed uk-table-hover'];
+	public $summaryOptions = ['class'=>'uk-text-center']; 
 	public $pager =  ['class'=> 'worstinme\uikit\widgets\LinkPager'];
 
 }
