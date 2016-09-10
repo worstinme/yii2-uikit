c9ca4d0f03fb40ba455b34a596c7e61464e8b28c
Evgeny Zakirov <box@flyleaf.su>
2016-09-11 00:40:00 +0800

    + timepicker

---
+++ assets/Timepicker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace worstinme\uikit\assets;
+
+use yii\web\AssetBundle;
+
+class Timepicker extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/uikit';
+
+    public $css = [
+        'css/components/timepicker.min.css',
+    ];
+
+    public $js = [
+        'js/components/timepicker.min.js',
+    ];
+
+    public $depends = [
+        'worstinme\uikit\UikitAsset',
+    ];
+}
