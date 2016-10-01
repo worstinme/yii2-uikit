5fd759f4eea6c0bcb5b5486a0eb356232f367c26
Evgeny Zakirov <box@flyleaf.su>
2016-10-01 19:08:36 +0800

    form select asset

---
+++ assets/FormSelect.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace worstinme\uikit\assets;
+
+use yii\web\AssetBundle;
+
+class FormSelect extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/uikit';
+
+    public $css = [
+        'css/components/form-select.min.css',
+    ];
+
+    public $js = [
+        'js/components/form-select.min.js',
+    ];
+
+    public $depends = [
+        'worstinme\uikit\UikitAsset',
+    ];
+}
