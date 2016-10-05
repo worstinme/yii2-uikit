ae7a7fee518c6518fdcf743825a07002361d3fb8
Evgeny Zakirov <box@flyleaf.su>
2016-10-05 15:40:56 +0800

    search component assets

---
+++ assets/Search.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace worstinme\uikit\assets;
+
+use yii\web\AssetBundle;
+
+class Search extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/uikit';
+
+    public $css = [
+        'css/components/search.min.css',
+    ];
+
+    public $js = [
+        'js/components/search.min.js',
+    ];
+
+    public $depends = [
+        'worstinme\uikit\assets\Autocomplete',
+    ];
+}
