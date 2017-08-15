b5da3a9d386362a7d282ab6dafee522bbdca3cee
portenko <portenko@gmail.com>
2017-08-15 15:22:51 +0500

    CRM-69 #comment тестирую 2-й гит

--- widgets/GridView.php
+++ widgets/GridView.php
@@ -4,7 +4,6 @@ namespace worstinme\uikit\widgets;
 
 class GridView extends \yii\grid\GridView
 {
-
 	public $tableOptions = ['class' => 'uk-table uk-form uk-table-condensed uk-table-hover'];
 	public $summaryOptions = ['class'=>'uk-text-center']; 
 	public $pager =  ['class'=> 'worstinme\uikit\widgets\LinkPager'];
